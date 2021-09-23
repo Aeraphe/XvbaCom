88e133e5b2c4de0dc22a7b23ea6cf0cc65a06866
Aeraphe <alberto.aeraph@gmail.com>
2021-09-23 20:52:34 -0300

    refactor: create separate file  for inport vba function

---
+++ XvbaImportExcelVbaFilescpp.cpp
@@ -0,0 +1,39 @@
+#include "XvbaCom.h"
+#include <iostream>
+#include "windows.h"
+
+
+
+int XvbaImportVBA(LPCTSTR szFilename) {
+
+
+    HRESULT hr;
+    IDispatch* pExcelCOM = (IDispatch*)NULL;
+    IDispatch* pWorkbook = (IDispatch*)NULL;
+    IDispatch* pVBAComponents = (IDispatch*)NULL;
+
+
+    try
+    {
+        //Create COM Instance
+        hr = XvbaCoCreateInstance(L"Excel.Application", pExcelCOM);
+        //Show Com
+        hr = XvbaShowApplication(pExcelCOM); 
+        //Open Document
+        hr = XvbaOpenDocument(szFilename,pExcelCOM,pWorkbook);
+
+
+
+    }
+    catch (const std::exception&)
+    {
+        pExcelCOM->Release();
+        return hr;
+    }
+
+
+    return hr;
+
+}
+
+
